0fc1b848a3b05deb2d6347926f6905bcfac4adfc
Dulluhan <yuanv@alumni.ubc.ca>
2015-09-10 22:00:29 -0700

    changed branch code to actual problem not solution

--- src/tutorial/src/cpp.cpp
+++ src/tutorial/src/cpp.cpp
@@ -1,64 +1,21 @@
 #include <iostream>
-#include <cstdlib>
-#include <vector>
+#include <ltcstdlib>
 
-#include <ros/ros.h>
-#include <std_msgs/String.h>
-
-#include <sstream>
-
-bool is_prime(unsigned int n){
-	if(n == 1) return false;
-	static std::vector<unsigned int> prev;
-	unsigned int start = prev.size() > 0 ? prev.back() : 2;
-	for(unsigned int i = start; i < n; ++i){
-		bool divides_by_any = false;
-		for(std::vector<unsigned int>::iterator it = prev.begin(); it != prev.end(); ++it){
-			if(i % *it == 0){
-				divides_by_any = true;
-				break;
-			}
+int main(){
+	for(int i = 0; i < 101; ++i){
+		bool fizzbuzz = false;
+		if(i % 3 == 0){
+			fizzbuzz = true;
+			std::cout << "fizz";
 		}
-		if(!divides_by_any){
-			prev.push_back(i);
+		if(i % 5 == 0){
+			fizzbuzz = true;
+			std::cout << "buzz";
 		}
+		if(!fizzbuzz){
+			std::cout << i;
+		}
+		std::cout << '\n';
 	}
-	for(std::vector<unsigned int>::iterator it = prev.begin(); it != prev.end(); ++it){
-		if(n % *it == 0){
-			return false;
-		}
-	}
-	return true;
-}
-
-int main(int argc, char** argv){
-	ros::init(argc, argv, "cpp");
-	ros::NodeHandle n;
-	ros::Publisher publisher = n.advertise<std_msgs::String>("primes", 1000);
-	for(int i = 1; i < 101; ++i){
-		std::ostringstream out;
-		if(is_prime(i)){
-			out << "prime";
-		}else{
-			bool fizzbuzz = false;
-			if(i % 3 == 0){
-				fizzbuzz = true;
-				out << "fizz";
-			}
-			if(i % 5 == 0){
-				fizzbuzz = true;
-				out << "buzz";
-			}
-			if(!fizzbuzz){
-				out << i;
-			}
-		}
-		std_msgs::String msg;
-		msg.data = out.str();
-		publisher.publish(msg);
-		ROS_INFO("%s\n", out.str().c_str());
-	}
-	ros::spin();
 	return EXIT_SUCCESS;
 }
-
