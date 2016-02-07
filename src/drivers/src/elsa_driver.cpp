adef16e8e058902ecee523b69ee1a48c3ebb57ca
Gareth Ellis <gareth.ellis0@gmail.com>
2016-02-07 11:06:05 -0800

    cleaned up conversion functions in driver, which now compiles fine

--- src/drivers/src/elsa_driver.cpp
+++ src/drivers/src/elsa_driver.cpp
@@ -34,15 +34,7 @@ static const char IDENTIFIER_BYTE = 'B';
 static const int SECOND = 1000000;
 
 
-// Converts a velocity command (-1 to 1) to an apm command (255 to 000)
-string velocityCommandToAPMCommand(double velocity){
-    // Convert velocity to value between 255 and 0
-    unsigned char apm_command = round((125 - (velocity * MOVE_RATE)));
-    char output[4] = {0};
-	snprintf(&output[0], 4, "%03u", apm_command);
-	return string(&output[0], 3);
-}
-
+// If rotation is greater or less then (-pi, pi), constrain it
 double bound_rotation(double rotation){
 	if(rotation > M_PI){
 		return M_PI;
@@ -53,35 +45,25 @@ double bound_rotation(double rotation){
 	}
 }
 
-int bounded_rotation_to_APMValue(double rotation_in_bounds){
-	// This scales rotation to whatever value is passed to iti
-	return 125 - round(TURN_RATE * rotation_in_bounds);
+// Converts a given integer command to one the apm can understand
+string commandToAPMCommand (int command){
+    // Convert the integer command to a 3 long string, adding zero's if needed
+  char output[4] = {0};
+  snprintf(&output[0], 4, "%03u", command);
+  return string(&output[0], 3);
 }
 
-int rotationToAPMValue(double rotation){
-	// ceiling and floor to max/min expected inputs
-	double rotation_in_bounds = bound_rotation(rotation);
-	int val_from_float = bounded_rotation_to_APMValue(rotation_in_bounds);
-
-	const unsigned char MIN_TURN = 125 - MAX_APM_TURN_RATE;
-	const unsigned char MAX_TURN = 125 + MAX_APM_TURN_RATE;
-	// ceiling/floor to max/min specified outputs
-	int apm_value;{
-		if(val_from_float < MIN_TURN){
-			apm_value = MIN_TURN;
-		}else if(val_from_float > MAX_TURN){
-			apm_value = MAX_TURN;
-		}else{
-			apm_value = val_from_float;
-		}
-	}
-	return apm_value;
+// Converts a rotation command to an apm command (255 to 000)
+string rotationCommandToAPMCommand(double rotation, double turn_rate_sensitivity){
+  int command = 125 - round(turn_rate_sensitivity * bound_rotation(rotation));
+  return commandToAPMCommand(command);
 }
 
-string rotationValueToAPMCommand(unsigned char value){
-	char output[4] = {0};
-	snprintf(&output[0], 4, "%03u", value);
-	return string(&output[0], 3);
+// Converts a velocity command (-1 to 1) to an apm command (255 to 000)
+string velocityCommandToAPMCommand(double velocity, double move_rate_sensitivity){
+    // Convert velocity to value between 255 and 0
+  unsigned char command = round((125 - (velocity * move_rate_sensitivity)));
+  return commandToAPMCommand(command);
 }
 
 int main(int argc, char** argv)
@@ -92,33 +74,41 @@ int main(int argc, char** argv)
   ros::NodeHandle private_nh("~");
 	ros::Rate loop_rate(ROS_LOOP_RATE);
 
+  //Set all values to neutral
+  char twist_Y[3]={'1','2','5'}; //Strafe (normally not used, robot has no lateral movement)
+  char twist_X[3]={'1','2','5'}; //Forward/Backward
+  char twist_z[3]={'1','2','5'}; //Rotation
+
     // Get any parameters
         // The maximum that the turn rate will go to
         // (ie. if max_turn_rate = 40, the max command sent to the apm would be 125 +/- 40 )
     double max_turn_rate = 40;
-    if (private_nh.getParam("max_turn_rate", max_turn_rate_param)){
-      max_turn_rate = max_turn_rate_param;
-    }
+    private_nh.param("max_turn_rate", max_turn_rate);
         // How sensitive the robot is to a given turn command
         // A higher value means that smaller commands will have a greater effect
+        // (ie. if turn_rate_sensitivity = 10, the apm command will be 125 + (turn_rate_sensitivity * given command))
     double turn_rate_sensitivity = 160;
-    if (private_nh.getParam("turn_rate_sensitivity", turn_rate_sensitivity_param)){
-      turn_rate_sensitivity = turn_rate_sensitivity_param;
-    }
-        // How sensitive the robot is to a given turn command
+    private_nh.param("turn_rate_sensitivity", turn_rate_sensitivity);
+        // How sensitive the robot is to a given movement (forward/backward) command
         // A higher value means that smaller commands will have a greater effect
-    double move_speed_sensitivity = 25;
-    if (private_nh.getParam("move_speed_sensitivity", move_speed_sensitivity_param)){
-      move_speed_sensitivity = move_speed_sensitivity_param;
-    }
+        // (ie. if move_rate_sensitivity = 10, the apm command will be 125 + (move_rate_sensitivity * given command))
+    double move_rate_sensitivity = 25;
+    private_nh.param("move_speed_sensitivity", move_rate_sensitivity);
+        // The USB port the apm is connected to
+    string usb_port_name = "/dev/ttyACM";
+    private_nh.param("usb_port_name", usb_port_name);
 
-    //Set all values to neutral
-    char twist_Y[3]={'1','2','5'}; //Strafe (normally not used, look up strafe)
-    char twist_X[3]={'1','2','5'}; //Forward/Backward
-    char twist_z[3]={'1','2','5'}; //Rotation
-    bool eStop = false;
+    //Set Subscribers and Publishers
+  	ros::Subscriber command_sub = public_nh.subscribe<geometry_msgs::Twist>
+      ("move_straight_line/command", 10, boost::function<void(geometry_msgs::Twist)>
+      ([&](geometry_msgs::Twist twist){
+  		// Write converted velocity and rotate commands to twist_Y and twist_z
+  		velocityCommandToAPMCommand(twist.linear.x, move_rate_sensitivity).copy(twist_X, 3, 0);
+  		rotationCommandToAPMCommand(twist.angular.z, turn_rate_sensitivity).copy(twist_z, 3, 0);
+      }));
 
 	//initialize serial communication
+  stringstream ss;
 	SerialCommunication link;
 	for (int i = 0; ; i++)
 	{
@@ -138,28 +128,21 @@ int main(int argc, char** argv)
 	usleep(10*SECOND);
   ROS_INFO("elsa_driver ready");
 
-	//subscribers and publishers
-	Subscriber command_sub = n.subscribe<geometry_msgs::Twist>("move_straight_line/command", 10, boost::function<void(geometry_msgs::Twist)>([&](geometry_msgs::Twist twist){
-		// Write converted velocity and rotate commands to twist_Y and twist_z
-		velocityCommandToAPMCommand(twist.linear.x).copy(twist_X, 3, 0);
-		rotationValueToAPMCommand(
-			rotationToAPMValue(twist.angular.z)).copy(twist_z, 3, 0);
-    }));
-
 	//While ROS is running
 	link.clearBuffer();
-	while(ok())
+	while(ros::ok())
 	{
 
 	    //write to apm
-	    stringstream ss;
-	    if (eStop)
-	    {
-		    cout << "eStop on" << endl;
-		    ss << (char)IDENTIFIER_BYTE << "125125125";
-	    } else {
-		    ss << (char)IDENTIFIER_BYTE<< twist_Y[0] << twist_Y[1] << twist_Y[2] << twist_X[0] << twist_X[1] << twist_X[2] << twist_z[0] << twist_z[1] << twist_z[2];
-	    }
+		  ss << (char)IDENTIFIER_BYTE<< twist_Y[0]
+                                  << twist_Y[1]
+                                  << twist_Y[2]
+                                  << twist_X[0]
+                                  << twist_X[1]
+                                  << twist_X[2]
+                                  << twist_z[0]
+                                  << twist_z[1]
+                                  << twist_z[2];
 	    link.writeData(ss.str(), 10);
 
 	    //publish data
@@ -167,7 +150,7 @@ int main(int argc, char** argv)
 	    link.readData(24, test);
   	    //cout << test;
 
-	    spinOnce();
+	    ros::spinOnce();
 	    loop_rate.sleep();
 	}
 
