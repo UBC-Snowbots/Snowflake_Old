38da0cc14fae485b847edbd813688029dee75d5d
ValRat <leri.ratu@gmail.com>
2016-02-10 23:17:15 -0800

    Git merging cleanup

--- src/opencv/src/color_analyzer.cpp
+++ src/opencv/src/color_analyzer.cpp
@@ -2,16 +2,21 @@
  * Analyzes an image and transforms it into a binary image
  * given some sort of colour specifiction
  * 
+ * Current method: Simple binary b/w separator from a set threshold
+ * Could attempt more sophisticated methods with more color channels
+ * or hues to deal with shade issue 
  */   
 
 #include <opencv2/core/core.hpp>
 #include <stdio.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
+
+void convertToBinary(const Mat &input, Mat &output);
 
 int main(int argc, char** argv){
     
@@ -23,6 +28,7 @@ int main(int argc, char** argv){
     }
 
     Mat inputImage;
+    Mat outputImage;
     
     //Camera information for user
     int width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
@@ -30,7 +36,7 @@ int main(int argc, char** argv){
     cout << "Frame size: " << width << " x " << height << endl;
     
     namedWindow("input", CV_WINDOW_AUTOSIZE);
-
+    namedWindow("output", CV_WINDOW_AUTOSIZE);
 
     while(1){
     
@@ -40,9 +46,11 @@ int main(int argc, char** argv){
             cout << "Failed to read image from camera" << endl;
             break;
         }
-
+        
+        convertToBinary(inputImage, outputImage);
+        
         imshow("input", inputImage);
-
+        imshow("output", outputImage);
         //Escape key to finish program
         if (waitKey(30) == 27){
             cout << "Escaped by user" << endl;
@@ -51,3 +59,7 @@ int main(int argc, char** argv){
     }
     return 0;
 }
+
+void convertToBinary(const Mat &input, Mat &output){
+    cvtColor(input, output, CV_BGR2GRAY);
+} 
