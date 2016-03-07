8aaf7e5f9efcab7c178f69413f420e648c3da4ea
Vincent Yuan <yuanv@alumni.ubc.ca>
2016-03-07 11:26:37 -0800

    Ground work for gps development set

---
+++ src/decision/src/sb_gps_manager/sb_gps.cpp
@@ -0,0 +1,13 @@
+/*
+* Node: sb_gps under decision 
+* Purpose: To make calculations using longitude and latitude data collected from serial from
+*          serial driver node 
+* Author: Vincent Yuan
+* Date: March 7, 2016
+*/
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+return 0; 
+}
---
+++ src/drivers/src/gps_driver.cpp
@@ -0,0 +1,10 @@
+/*
+* Node: sb_gps_driver in driver node
+* Purpose: Collecting gps/compass data via serial 
+* Author: Vincent Yuan 
+* Date: March 7, 2016
+*/
+#include <stdlib.h>
+
+
+int main (int argc, char **argv){}
