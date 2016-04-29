e30778bcb082fa33295113aad1c1620c7d2a9cbc
gareth <gareth.ellis0@gmail.com>
2016-04-29 12:33:39 -0700

    Point cloud testing

--- src/opencv/src/imageToCloud.cpp
+++ src/opencv/src/imageToCloud.cpp
@@ -111,7 +111,7 @@ int main (int argc, char** argv){
 		}
 
         //Publishes the cloud
-		cloud.header.stamp = ros::Time::now().toNSec();
+		//cloud.header.stamp = ros::Time::now().toNSec();
 		pub.publish(cloud_ptr);
 		
     
