867598528f3e012764508a762131d503d3844b62
Jagjot Jhajj <jhajj96@gmail.com>
2016-04-29 15:52:28 -0700

    Added the list manager along with some tests for it

---
+++ src/jaus/jaus/list_manager.py
@@ -0,0 +1,213 @@
+﻿
+"""
+The list manager is a doubly linked list that can contain a cycle, thus there is a possibility of one node having 
+two previous nodes
+
+The manager contains the following functions:
+
+setElement: Adds an element to the list, as long as it is a valid request
+
+deleteElement: Removes an element from the list, as long as the request is valid
+
+getElementSet: Returns a set of all elements in the list
+
+getHead: Returns the head of the list, the node which has a previous node with UID of 0
+
+getElement: Takes in a UID and returns an element with that UID
+
+
+I don't know exactly how to return a 'RejectElementRequest' so that will need to be implemented
+later, for now I just added a comment
+
+Known errors:
+Deleting an element in 1->2->3->2 does not work if removing the element 3, however
+trying to remove 2 will fail as intended
+"""
+class ListManager:
+    def __init__(self):
+        self.element_set = set()
+
+
+    def setElement(self, element):
+        
+        #Do not allow elements with multiple previous nodes to be set, or element with UID 0
+        if(len(element.previousList) > 1 or element.UID == 0):
+            """Return a RejectElementRequest here"""
+            return
+
+        #"""Case 1 : Element is being added to an empty list"""
+        if(len(self.element_set) == 0):
+            if(element.isPrevious(0) and element.nextUID == 0):
+               self.element_set.add(element)
+               return
+
+        #This is a check for if the element already exists in the list
+        for e in self.element_set:
+            if (e.UID == element.UID):
+                """Return a RejectElementRequest here"""
+                return
+
+        #"""Case 2: Element is being added to the beginning of the list"""
+        if(element.isPrevious(0)):
+            for e in self.element_set:
+                if(e.isPrevious(0) and element.nextUID == e.UID()): 
+                    e.setPrevious(element.UID)
+                    self.element_set.add(element)
+                    break
+
+        #"""Case 3: Element is being added to the end of the list"""
+        elif(element.nextUID == 0):
+            for e in self.element_set:
+                if(e.nextUID == 0 and element.isPrevious(e.UID)):
+                    e.setNext(element.UID)
+                    self.element_set.add(element)
+                    break
+
+        #"""Case 4: Element is being added between two existing nodes"""
+        else:
+            previousElement = None
+            nextElement = None
+           
+            for e in self.element_set:
+                if(element.isPrevious(e.UID)):
+                    previousElement = e
+                if(element.nextUID == e.UID):
+                    nextElement = e
+
+            if(previousElement != None and nextElement != None):
+                
+                #Case 4.1: Element is not creating a circular loop
+                if(nextElement.UID == previousElement.nextUID): 
+                    previousElement.setNext(element.UID)
+                    nextElement.addPrevious(element.UID)
+                    nextElement.removePrevious(previousElement.UID)
+                    
+                    self.element_set.add(element)
+
+                #Case 4.2: Element is creating a circular loop
+                elif(previousElement.nextUID == 0):
+                    previousElement.setNext(element.UID)
+                    nextElement.addPrevious(element.UID)
+                    self.element_set.add(element)
+
+    def deleteElement(self, element):
+
+        #"""Case 1: Element is not in the list"""
+        if element not in self.element_set:
+            """Return a "RejectElementRequest" message here"""
+            return 
+
+        #"""Case 2: There is 1 element in the list"""
+        if(len(self.element_set) == 1):
+            self.element_set.remove(element)
+            return
+
+        nextElement = None
+        previousElement = None
+
+        #"""Case 3: Element to be removed is the head of the list"""
+        if(element.isPrevious(0)):
+            for e in self.element_set:
+                if(element.nextUID == e.UID):
+                    nextElement = e
+
+            if(nextElement == None):
+                print("The code really should never have reached this point...")
+                """Return a "RejectElementRequest" here"""
+                return
+
+            nextElement.removePrevious(element.UID)
+            self.element_set.remove(element)
+
+        #"""Case 4: Element to be removed is at the end of the list"""
+
+        elif(element.nextUID == 0):
+            for e in self.element_set:
+                if(element.isPrevious(e.UID)):
+                    previousElement = e
+
+            if(previousElement == None):
+                print("The code really should never have reached this point...")
+                """Return a "RejectElementRequest" here"""
+                return
+
+            previousElement.setNext(0)
+            self.element_set.remove(element)
+
+        #"""Case 5: Element to be removed is somewhere in the middle of the list"""
+        else:
+            previousList = []
+            nextElement = None
+
+            for e in self.element_set:
+                if(element.isPrevious(e.UID)):
+                    previousList.append(e)
+                if(element.nextUID == e.UID):
+                    nextElement = e
+            
+            
+            if(nextElement in previousList):
+                """retun RejectElementRequest here"""
+                return
+
+            nextElement.clearPreviousList()
+            for p in previousList:
+                p.setNext(element.nextUID)
+                nextElement.addPrevious(p.UID)
+
+           
+            self.element_set.remove(element)
+
+    def getElementSet(self):
+        return self.element_set
+
+    def getHead(self):
+        for e in self.element_set:
+            if(e.isPrevious(0)):
+                return e
+        return None  
+    
+    def getElement(self, UID):
+        for e in self.element_set:
+            if(e.UID == UID):
+                return e
+        return None
+         
+class ListElement:
+    """
+    A list is used for previous elements since, in a circular list, 
+    a node can have more than one previous element
+    """
+    def __init__(self, UID, previousUID, nextUID):
+        self.previousList = [previousUID]
+        self.UID = UID
+        self.nextUID = nextUID
+        
+    def setNext(self, next):
+        self.nextUID = next
+
+    def setPrevious(self, previous):
+        self.previousList = [previous]
+
+    def setPreviousList(self, list):
+        self.previousList = list
+
+    def addPrevious(self, previous):
+        self.previousList.append(previous)
+        if(0 in self.previousList):
+            previousList.remove(0)
+
+    def clearPreviousList(self):
+        self.previousList = []
+
+    def isLoopStart(self):
+        return (len(self.previousList) == 2)
+
+    def isPrevious(self, element):
+        return (element in self.previousList)
+
+    def removePrevious(self, id):
+        self.previousList.remove(id)
+        if(len(self.previousList) == 0):
+            self.previousList.append(0)
+    
---
+++ src/jaus/tests/test_list_manager.py
@@ -0,0 +1,300 @@
+﻿import pytest
+
+from jaus.list_manager import(
+
+    ListManager,
+    ListElement
+)
+
+#Test that inserting an element into an empty List works as intended
+def test_first_insertion():
+
+    list_manager = ListManager()
+
+    new_element = ListElement(5,0,0)
+
+    list_manager.setElement(new_element)
+
+    assert(new_element in list_manager.getElementSet())
+
+
+#Tests the following case: Add a head element, add a tail element, then add an element in between the head and tail
+def test_multiple_insertions():
+
+    list_manager = ListManager()
+
+    head_element = ListElement(1,0,0)
+    tail_element = ListElement(2,1,0)
+    middle_element = ListElement(3,1,2)
+
+    list_manager.setElement(head_element)
+    list_manager.setElement(tail_element)
+    list_manager.setElement(middle_element)
+
+    assert(head_element in list_manager.getElementSet())
+    assert(tail_element in list_manager.getElementSet())
+    assert(middle_element in list_manager.getElementSet())
+    
+    list_head = list_manager.getHead()
+    assert(list_head == head_element)
+
+    list_second = list_manager.getElement(list_head.nextUID)
+    assert(list_second == middle_element)
+
+    list_third = list_manager.getElement(list_second.nextUID)
+    assert(list_third == tail_element)
+
+    assert(list_third.nextUID == 0)
+
+def test_head_element_deletion():
+
+    list_manager = ListManager()
+
+    head_element = ListElement(1,0,0)
+    tail_element = ListElement(2,1,0)
+    middle_element = ListElement(3,1,2)
+
+    list_manager.setElement(head_element)
+    list_manager.setElement(tail_element)
+    list_manager.setElement(middle_element)
+
+    list_manager.deleteElement(head_element)
+
+    assert(len(list_manager.getElementSet()) == 2)
+
+    assert(list_manager.getHead() == middle_element)
+    
+    list_second = list_manager.getElement(list_manager.getHead().nextUID)
+    assert(list_second == tail_element)
+
+    assert(list_second.nextUID == 0)
+
+def test_tail_element_deletion():
+    list_manager = ListManager()
+
+    head_element = ListElement(1,0,0)
+    tail_element = ListElement(2,1,0)
+    middle_element = ListElement(3,1,2)
+
+    list_manager.setElement(head_element)
+    list_manager.setElement(tail_element)
+    list_manager.setElement(middle_element)
+
+    list_manager.deleteElement(tail_element)
+
+    assert(len(list_manager.getElementSet()) == 2)
+
+    assert(list_manager.getHead() == head_element)
+    
+    list_second = list_manager.getElement(list_manager.getHead().nextUID)
+    assert(list_second == middle_element)
+
+    assert(list_second.nextUID == 0)
+
+
+
+def test_middle_element_deletion():
+    list_manager = ListManager()
+
+    head_element = ListElement(1,0,0)
+    tail_element = ListElement(2,1,0)
+    middle_element = ListElement(3,1,2)
+
+    list_manager.setElement(head_element)
+    list_manager.setElement(tail_element)
+    list_manager.setElement(middle_element)
+
+    list_manager.deleteElement(middle_element)
+
+    assert(len(list_manager.getElementSet()) == 2)
+
+    assert(list_manager.getHead() == head_element)
+    
+    list_second = list_manager.getElement(list_manager.getHead().nextUID)
+    assert(list_second == tail_element)
+
+    assert(list_second.nextUID == 0)
+
+def test_complete_deletion():
+    list_manager = ListManager()
+
+    head_element = ListElement(1,0,0)
+    tail_element = ListElement(2,1,0)
+    middle_element = ListElement(3,1,2)
+
+    list_manager.setElement(head_element)
+    list_manager.setElement(tail_element)
+    list_manager.setElement(middle_element)
+
+    list_manager.deleteElement(middle_element)
+    list_manager.deleteElement(head_element)
+
+    assert(len(list_manager.getElementSet()) == 1)
+
+    assert(list_manager.getHead() == tail_element)
+    list_manager.deleteElement(tail_element)
+
+    assert(len(list_manager.getElementSet()) == 0)
+   
+def test_circular_list():
+    list_manager = ListManager()
+
+    first_element = ListElement(1,0,0)
+    second_element = ListElement(2,1,0)
+    third_element = ListElement(3,2,0)
+    fourth_element = ListElement(4,3,2)
+
+    list_manager.setElement(first_element)
+    list_manager.setElement(second_element)
+    list_manager.setElement(third_element)
+    list_manager.setElement(fourth_element)
+
+
+
+    fifth_element = ListElement(5,4,2)
+    list_manager.setElement(fifth_element)
+
+    list_head = list_manager.getHead()
+    assert(list_head == first_element)
+
+    list_second = list_manager.getElement(list_head.nextUID)
+    assert(list_second == second_element)
+
+    assert list_second.isLoopStart()
+
+    list_third = list_manager.getElement(list_second.nextUID)
+    assert(list_third == third_element)
+
+    list_fourth = list_manager.getElement(list_third.nextUID)
+    assert(list_fourth == fourth_element)
+    
+    list_fifth = list_manager.getElement(list_fourth.nextUID)
+    assert(list_fifth == fifth_element)
+
+    assert( list_manager.getElement(list_fifth.nextUID) == list_second)
+  
+def test_circular_deletion():
+    list_manager = ListManager()
+
+    first_element = ListElement(1,0,0)
+    second_element = ListElement(2,1,0)
+    third_element = ListElement(3,2,0)
+    fourth_element = ListElement(4,3,2)
+
+    list_manager.setElement(first_element)
+    list_manager.setElement(second_element)
+    list_manager.setElement(third_element)
+    list_manager.setElement(fourth_element)
+    
+
+    list_manager.deleteElement(second_element)
+
+    list_head = list_manager.getHead()
+    assert(list_head == first_element)
+
+    list_second = list_manager.getElement(list_head.nextUID)
+    assert(list_second == third_element)
+
+    list_third = list_manager.getElement(list_second.nextUID)
+    assert(list_third == fourth_element)
+
+    list_fourth = list_manager.getElement(list_third.nextUID)
+    assert(list_fourth == third_element)
+
+
+#Tests that elements which point to themselves are rejected
+
+def test_self_circular():
+
+    list_manager = ListManager()
+
+    first_element = ListElement(1,1,1)
+    
+    assert(len(list_manager.getElementSet()) == 0)
+
+
+    first_element = ListElement(1,0,0)
+    second_element = ListElement(2,1,0)
+    third_element = ListElement(3,2,0)
+    fourth_element = ListElement(4,3,2)
+
+    list_manager.setElement(first_element)
+    list_manager.setElement(second_element)
+    list_manager.setElement(third_element)
+    list_manager.setElement(fourth_element)
+
+    list_manager.deleteElement(second_element)
+    list_manager.deleteElement(third_element) #Expect this deletion to fail
+
+    list_head = list_manager.getHead()
+    assert(list_head == first_element)
+
+    list_second = list_manager.getElement(list_head.nextUID)
+    assert(list_second == third_element)
+
+    list_third = list_manager.getElement(list_second.nextUID)
+    assert(list_third == fourth_element)
+
+    list_fourth = list_manager.getElement(list_third.nextUID)
+    assert(list_fourth == third_element)
+    
+""""
+Tests multiple requests that should be rejected, such as:
+
+-Inserting an element which already exists
+
+
+The list will begin (and should end) in the following form:
+    17->83->21->4->18->26->21 (circular)
+"""
+def test_multiple_rejections():
+    
+    list_manager = ListManager()
+
+    first_element = ListElement(17,0,0)
+    second_element = ListElement(83,17,0)
+    third_element = ListElement(21,83,0)
+    fourth_element = ListElement(4,21,0)
+    fifth_element = ListElement(18,4,0)
+    sixth_element = ListElement(26,18,21)
+
+    list_manager.setElement(first_element)
+    list_manager.setElement(second_element)
+    list_manager.setElement(third_element)
+    list_manager.setElement(fourth_element)
+    list_manager.setElement(fifth_element)
+    list_manager.setElement(sixth_element)
+
+
+
+    #Now it's time for the requests which need to be rejected
+
+    repeatedElement = ListElement(83,4,18)
+    list_manager.setElement(repeatedElement) #Add element with an ID that already exists
+
+
+    secondLoopElement = ListElement(211,4,21)
+    list_manager.setElement(secondLoopElement) #Add a second loop
+
+
+
+    list_head = list_manager.getHead()
+    assert(list_head == first_element)
+
+    list_second = list_manager.getElement(list_head.nextUID)
+    assert(list_second == second_element)
+
+    list_third = list_manager.getElement(list_second.nextUID)
+    assert(list_third == third_element)
+
+    list_fourth = list_manager.getElement(list_third.nextUID)
+    assert(list_fourth == fourth_element)
+
+    list_fifth = list_manager.getElement(list_fourth.nextUID)
+    assert(list_fifth == fifth_element)
+
+    list_sixth = list_manager.getElement(list_fifth.nextUID)
+    assert(list_sixth == sixth_element)
+
+    list_seventh = list_manager.getElement(list_sixth.nextUID)
+    assert(list_seventh == third_element)
