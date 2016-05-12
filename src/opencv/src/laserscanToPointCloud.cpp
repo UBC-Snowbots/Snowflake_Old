d027afef44ca19d01196c0411f9fa6a9a8e8a150
Snowbots Laptop <snowbots.ubc@gmail.com>
2016-05-12 15:26:08 -0700

    created a node to convert from laserscan's to pcl::PointCloud<pcl::PointXYZ> rather then sensor_msgs/PointCloud2

--- src/opencv/src/laserscanToPointCloud.cpp
+++ src/opencv/src/laserscanToPointCloud.cpp
@@ -5,6 +5,7 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <math.h>
 
 using namespace pcl;
 using namespace std;
@@ -26,6 +27,7 @@ private:
     string cloud_topic;
 
     sensor_msgs::LaserScan scan;
+    
 };
 
 LaserScanToPointCloud::LaserScanToPointCloud() {
@@ -34,8 +36,9 @@ LaserScanToPointCloud::LaserScanToPointCloud() {
     nh.getParam("scan_topic", scan_topic);
     nh.getParam("cloud_topic", cloud_topic);
     // Start the publisher
-    pointcloud_publisher_ = node_.advertise<sensor_msgs::PointCloud2> (cloud_topic.c_str(), 1, false);
-
+    pointcloud_publisher_ = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>(cloud_topic.c_str(), 1);
+     
+    
     this->scan_topic_parser();
 }
 
@@ -54,9 +57,40 @@ void LaserScanToPointCloud::scan_topic_parser(){
 
 void LaserScanToPointCloud::scanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan, std::string topic){
     // Convert laserscan to a pointcloud and publish it
+    // Convert using laser_geometry library
+    /*
     sensor_msgs::PointCloud2 cloud;
     projector_.projectLaser(*scan, cloud);
     pointcloud_publisher_.publish(cloud);
+    */
+    // Convert using in house conversion method 
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr(&cloud);  
+     
+    cloud.clear();
+
+    //Create the cloud and its points
+    cloud.header.frame_id = "laser";
+    cloud.width = scan->ranges.size();
+    cloud.height = 1;
+    cloud.is_dense = false;
+    cloud.points.resize(cloud.width * cloud.height);
+
+    for (int i =0; i < scan->ranges.size(); i++){
+        //ROS_INFO("%d", i);
+        // Make sure the scan point is within the acceptable ranges of the scan
+        if ((scan->ranges[i] > scan->range_min) && (scan->ranges[i] < scan->range_max)){
+            // Create a new point and add it to the cloud
+            pcl::PointXYZ point;
+            // YES THESE CALCULATIONS ARE SUPPOSED TO BE INVERTED, it's just how ROS's coordinate system works
+            float theta = scan->angle_min + (i * scan->angle_increment);
+            point.x = (float) scan->ranges[i] * cos(theta);
+            point.y = (float) scan->ranges[i] * sin(theta);
+            point.z = (float) 0.1;
+            cloud.push_back(point);
+        }
+    }
+    pointcloud_publisher_.publish(cloud.makeShared());
 }
 
 int main(int argc, char** argv){
