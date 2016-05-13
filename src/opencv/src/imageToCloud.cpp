e95c41dca5c2035b327500358d32a201df269a37
Gareth <gareth.ellis0@gmail.com>
2016-05-12 17:38:38 -0700

    refactored into a class to allo implementation of on the fly calibration (not done yet)

--- src/opencv/src/imageToCloud.cpp
+++ src/opencv/src/imageToCloud.cpp
@@ -37,110 +37,75 @@ const int NUM_ELTS_SKIPPED = 2;
 const int MAX_HEIGHT = 20;
 const int SCALE_FACTOR = 100;
 
-Mat inputImage;
+class ImageToCloud {
+    public:
+        ImageToCloud();
+        void imageCallBack(const sensor_msgs::ImageConstPtr& msg, std::string topic);
+    private:
+        Mat inputImage = Mat::zeros(480, 640, CV_8UC1);
+	    ros::Publisher pub;
+        image_transport::Subscriber sub;
+};
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-  try
-  {
-    inputImage = cv_bridge::toCvShare(msg, "mono8")->image;
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
-  }
-}
-
-int main (int argc, char** argv){
-
-    //Ros initialization
-	ros::init(argc, argv, "image_to_cloud_node");
+ImageToCloud::ImageToCloud(){ 
 	ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("image", 1, imageCallback);
+    sub = it.subscribe("image", 1, boost::bind(&ImageToCloud::imageCallBack, this, _1, "image"));
 	string topic = nh.resolveName("point_cloud");
 	uint32_t queue_size = 1;
 	ros::Rate loop_rate(5);
-	ros::Publisher pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>(topic, queue_size);
+	pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>(topic, queue_size);
+    bool isCalibratingManually = false;
+}
 
-    //Creating the viewer object
-    pcl::visualization::PCLVisualizer viewer("3D Viewer");
-    viewer.setBackgroundColor (0, 0, 0);
-    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "input");
-    viewer.addCoordinateSystem (1.0);
-    viewer.initCameraParameters ();
-    
-    bool cloudSet = false;
 
-    //Cloud and pointer initialization
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr(&cloud);
+void ImageToCloud::imageCallBack(const sensor_msgs::ImageConstPtr& msg, std::string topic)
+{
+    try
+    {
+        inputImage = cv_bridge::toCvShare(msg, "mono8")->image;
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+    }
 
-    //Empty Image initializatoin
-    inputImage = Mat::zeros(480, 640, CV_8UC1);
-	
-    while (nh.ok()){
+    pcl::PointCloud<pcl::PointXYZ> cloud;    
+    //Isn't needed? Questionable
+    cloud.clear();
 
-        //Isn't needed? Questionable
-        cloud.clear();
+    //Creating the cloud and its points
+    cloud.header.frame_id = "map";
+    cloud.width = inputImage.cols * inputImage.rows;
+    cloud.height = 1;
+    cloud.is_dense = false;
+    cloud.points.resize(cloud.width * cloud.height);
 
-        //Creating the cloud and its points
-		cloud.header.frame_id = "map";
-		cloud.width = inputImage.cols * inputImage.rows;
-		cloud.height = 1;
-		cloud.is_dense = false;
-		cloud.points.resize(cloud.width * cloud.height);
-
-		for (int row = 0; row < inputImage.rows; row = row + NUM_ELTS_SKIPPED){
-			for (int col = 0; col < inputImage.cols; col = col + NUM_ELTS_SKIPPED){
-				if (inputImage.at<uchar>(row, col) > 0){
-                    for (int i = 0; i < MAX_HEIGHT; i++){
-                        pcl::PointXYZ point;
-                        point.y = (float) -(col - (inputImage.cols/2)) /SCALE_FACTOR;
-                        point.x = (float) -(row - inputImage.rows)/SCALE_FACTOR;
-					    point.z = (float) i/SCALE_FACTOR;
-                        cloud.push_back(point);
-                    }
-				} 
-			}
-		}
-
-        //Publishes the cloud
-		//cloud.header.stamp = ros::Time::now().toNSec();
-		pub.publish(cloud_ptr);
-		
-    
-		//Escape key to finish program
-        int a = waitKey(20);
-        if (a == 27){
-            cout << "Escaped by user" << endl;
-            break;
+    for (int row = 0; row < inputImage.rows; row = row + NUM_ELTS_SKIPPED){
+        for (int col = 0; col < inputImage.cols; col = col + NUM_ELTS_SKIPPED){
+            if (inputImage.at<uchar>(row, col) > 0){
+                for (int i = 0; i < MAX_HEIGHT; i++){
+                    pcl::PointXYZ point;
+                    point.y = (float) -(col - (inputImage.cols/2)) /SCALE_FACTOR;
+                    point.x = (float) -(row - inputImage.rows)/SCALE_FACTOR;
+                    point.z = (float) i/SCALE_FACTOR;
+                    cloud.push_back(point);
+                }
+            } 
         }
+    }
 
-        //Show the cloud in the viewer
-        if (!cloudSet){
-            viewer.addPointCloud(cloud_ptr, "input");
-            cloudSet = true;
-        } else{
-            viewer.updatePointCloud(cloud_ptr, "input"); 
-        }
-
-        //Needed to update the viewer and to keep ros working
-		viewer.spinOnce();
-		ros::spinOnce();
-	}
-
-	/* Iterators hard to get x,y value
-	MatIterator_<uchar> it = image.begin<uchar>();
-	MatIterator_<uchar> itend = image.end<uchar>();
-
-	for (;it != itend; it++){
-		int a = (int) (*it);
-		cout << a << " ";
-	}
-	*/
+    //Publishes the cloud
+    pub.publish(cloud.makeShared());
+}
 
 
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "image_to_cloud");
 
-	return 0;
+    ImageToCloud _image_to_cloud;
+
+    ros::spin();
+
+    return 0;
 }	
